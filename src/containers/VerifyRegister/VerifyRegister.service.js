90e4240d3aaf54fb0b14ea0c2d143e0091360d1d
Tran Nguyen Long <son.tran@nal.vn>
2021-07-25 17:22:15 +0700

    [FE] Fix bug register user

--- src/containers/VerifyRegister/VerifyRegister.service.js
+++ src/containers/VerifyRegister/VerifyRegister.service.js
@@ -1,9 +1,9 @@
 import ApiHelper from "../../configs/api/api-helper";
 
 export default class VerifyRegisterService extends ApiHelper {
-    verify(id,code){
-       const url = '/api/v1/register/activation/' + id + `?code=${code}`;
-       return this.post(url);
+    static verify(id, code) {
+        const url = '/api/v1/register/activation/' + id + `?code=${code}`;
+        return this.post(url);
 
     }
 }
--- src/utils/HeaderUtil.js
+++ src/utils/HeaderUtil.js
@@ -1,10 +1,10 @@
-
-const pathNotIncludeHeader = ['login','register','404'];
+const pathNotIncludeHeader = ['login', 'register', '404', 'verify-register'];
 
 
 export const checkPathIsIncludeHeader = location => {
- console.log(pathNotIncludeHeader.includes(location.pathname.substring(1)))
- return pathNotIncludeHeader.includes(location.pathname.substring(1)) ;
+    console.log('path', location.pathname.substring(1));
+    console.log(pathNotIncludeHeader.includes(location.pathname.substring(1)))
+    return pathNotIncludeHeader.includes(location.pathname.substring(1));
 }
 
 
